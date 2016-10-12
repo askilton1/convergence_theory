3add0b550685200b3514096261ec3a317e49c5b1
askilton1 <antonio.skilton@gmail.com>
2016-10-11 22:53:11 -0700

    changed units of gdpPercap and popMill (formerly popThous)
    also moved character cleaning within dplyr chain

--- clean.R
+++ clean.R
@@ -2,7 +2,10 @@
 library(antonioSkiltonTools);library(dplyr);library(tidyr);library(ggplot2);library(stringr);library(reshape2)
 flash <- tbl_df(read.csv("4_Flash_Proj_1_Data.csv"))
 
-flash$country %>%
-  str_replace_all(.,"[.,]","") %>%
-  str_replace_all(.," ","_") %>%
-  as.factor -> flash$country
+flash %>%
+  mutate(country = str_replace_all(country,"[.,]",""),
+         country = str_replace_all(country," ","_"),
+         country = as.factor(country),
+         gdpPercap = gdpPercap/1000,
+         popMill = popThous/1000) %>% 
+  select(-popThous) -> flash
